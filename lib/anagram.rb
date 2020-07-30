21a56db819641e62f30b5fc8a159fdc53454a245
Victor Avila <avilavictor1570@gmail.com>
2020-07-30 03:09:10 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,6 +2,12 @@ class Anagram
   
   attr_accessor :word
   
-  def match(word)
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(array_of_words)
+    array_of_words.select do |element|(@word.split("").sort) == (element.split("").sort)
+    end
     
 end
